716047254e31e888fe545da42b2757afeae13f5b
GuanyunLu <guanyun_hdu@outlook.com>
2022-04-22 18:58:00 +0800

    feat: scripts implemented

--- server_get.js
+++ server_get.js
@@ -1,11 +1,11 @@
-import fs from "fs";
+const fs=require('fs');
 let results = new Map();
-fs.readdirSync("./").forEach(function (folder) {
-  let stat = fs.statSync(folder);
+fs.readdirSync("../pastshows").forEach(function (folder) {
+  let stat = fs.statSync(`../pastshows/${folder}`);
   if (stat && stat.isDirectory())
     results.set(
       `${folder}`,
-      fs.readFileSync(`${folder}/text.txt`, "utf-8").replace(/\n/g, "<br/>")
+      fs.readFileSync(`../pastshows/${folder}/text.txt`, "utf-8").replace(/\n/g, "<br/>")
     );
 });
 fs.writeFileSync("result.json", JSON.stringify(Array.from(results.entries())));
